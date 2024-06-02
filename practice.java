aa1ab0987223e82507936d9bba0d978c53be9caf
Sahil Behaniya <121475283+SahilBehaniya@users.noreply.github.com>
2024-06-02 17:00:33 +0530

    Add files via upload

---
+++ practice.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class practice{
+   
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter the int");
+    int []x=new int[5];
+    for(int i=0;i<5;i++){
+        x[i]=sc.next();
+
+    }   
+
+    for(int i=0;i<5;i++){
+        System.out.println(x[i]);
+    }
+}
+}
